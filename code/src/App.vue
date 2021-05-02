c29b1554801a5c0939b387f528923ff6d7b6003c
Thamer <thamer.allogmani@gmail.com>
2021-05-03 02:17:09 +0300

    add custuomButton

--- code/src/App.vue
+++ code/src/App.vue
@@ -1,22 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<custom-button text="Register"
+               color="black"
+               background-color="white"
+               :disabled="valid"
+/>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+<script>
+import CustomButton from "./components/CustomButton.vue";
+export default {
+  components:{CustomButton},
+  data(){
+    return {valid:true}
+  }
+}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
---
+++ code/src/components/CustomButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button :style="{color,backgroundColor}" :disabled="disabled">{{text}}</button>
+</template>
+
+<script>
+export default {
+name: "CustomButton",
+  props:{
+    text:{type: String , required: true},
+    color:{type:String},
+    backgroundColor:{type:String},
+    disabled:{type:Boolean , required: true},
+
+  }
+}
+</script>
+
+<style scoped>
+button:disabled{
+  opacity:.5 ;
+}
+</style>
